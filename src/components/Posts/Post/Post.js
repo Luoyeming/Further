524b0dd578bf2b9b11888c966679a5d03d1adee1
Luoyeming <1378319493@qq.com>
2023-04-03 01:45:25 +0800

    修改post

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -3,10 +3,14 @@ import moment from "moment";
 import {useDispatch} from "react-redux";
 import {deletePost,likePost} from "../../../actions/posts";
 import {Link} from "react-router-dom";
+import { PlusOutlined, HeartFilled, ArrowDownOutlined } from '@ant-design/icons';
+import {Avatar} from 'antd'
+import './style.css'
 
 const Post = ({post,setCurrentId}) => {
     const dispatch = useDispatch();
     const user = JSON.parse(localStorage.getItem('profile'));
+    console.log(post)
     const Likes = () => {
         if(post.likes.length > 0){
             return post.likes.find((like) => like === (user?.result?._id))
@@ -25,30 +29,46 @@ const Post = ({post,setCurrentId}) => {
         return <><i className="fa fa-thumbs-o-up fa-lg"></i>Like</>
     }
     return (
-        <div className={post?"w-full block relative shadow-inner":"w-full block relative loading"}>
-            <img src={`${post.selectFile}`} className="rounded-t-2xl"/>
-            <div className="absolute left-2 top-2 text-xl text-gray-900">
-                <h1>{moment(post.createdAt).format('MMMM Do YYYY, h:mm:ss a')}</h1>
-                <h1>{post.name}</h1>
-            </div>
-            {(user?.result?._id === post?.creator) &&(
-                <Link to="/form"><button className=" absolute right-2 top-2" onClick={() => setCurrentId(post._id)}><i className="fa fa-ellipsis-h fa-2x text-gray-200"></i></button></Link>
-/*
-                <button className=" absolute right-2 top-2" onClick={() => setCurrentId(post._id)}><i className="fa fa-ellipsis-h fa-2x text-gray-200"></i></button>
-*/
+        <div className="post">
+            <img src={`${post.selectFile}`} />
+            {/*<div className="absolute left-2 top-2 text-xl text-gray-900">*/}
+            {/*    <h1>{moment(post.createdAt).format('MMMM Do YYYY, h:mm:ss a')}</h1>*/}
+            {/*    <h1>{post.name}</h1>*/}
+            {/*</div>*/}
+            {/*{(user?.result?._id === post?.creator) &&(*/}
+            {/*    <Link to="/form"><button className=" absolute right-2 top-2" onClick={() => setCurrentId(post._id)}><i className="fa fa-ellipsis-h fa-2x text-gray-200"></i></button></Link>*/}
 
-                )}
-            <div className="bg-gray-100 rounded-b-2xl">
-                <p className="p-2  text-gray-700">{post.tags.map((tag) => `#${tag}`)}</p>
-                <h1 className="p-1 text-2xl text-gray-900">{post.title}</h1>
-                <p className="p-2 text-xl text-gray-900 description">{post.message}</p>
-                <div className="px-3 text-gray-900 pb-2">
-                    <button className="text-base" disabled={!user?.result} onClick={() => dispatch(likePost(post._id))}>
-                       <Likes/>
-                    </button>
-                    {(user?.result?._id === post?.creator) &&(
-                    <button className="float-right text-base" onClick={() => dispatch(deletePost(post._id))}><i className="fa fa-close fa-lg"></i>删除</button>
-                    )}
+            {/*    )}*/}
+            {/*<div className="bg-gray-100 rounded-b-2xl">*/}
+            {/*    <p className="p-2  text-gray-700">{post.tags.map((tag) => `#${tag}`)}</p>*/}
+            {/*    <h1 className="p-1 text-2xl text-gray-900">{post.title}</h1>*/}
+            {/*    <p className="p-2 text-xl text-gray-900 description">{post.message}</p>*/}
+            {/*    <div className="px-3 text-gray-900 pb-2">*/}
+            {/*        <button className="text-base" disabled={!user?.result} onClick={() => dispatch(likePost(post._id))}>*/}
+            {/*           <Likes/>*/}
+            {/*        </button>*/}
+            {/*        {(user?.result?._id === post?.creator) &&(*/}
+            {/*        <button className="float-right text-base" onClick={() => dispatch(deletePost(post._id))}><i className="fa fa-close fa-lg"></i>删除</button>*/}
+            {/*        )}*/}
+            {/*    </div>*/}
+            {/*</div>*/}
+            <div className="likeAndColl">
+                <div className="icon" >
+                    <HeartFilled/>
+                </div>
+                <div className="icon" >
+                    <PlusOutlined/>
+                </div>
+            </div>
+            <div className="creatorAndDownLoad">
+                <div className="creator">
+                    <Avatar  src={`${post.creatorFile}`} />
+                    <span>{post?.name}</span>
+                </div>
+                <div className="downLoad">
+                    <div className="icon" >
+                        <ArrowDownOutlined />
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -2,12 +2,18 @@ import React from 'react';
 import Post from "./Post/Post";
 import {useSelector} from "react-redux";
 import Masonry from 'react-responsive-masonry'
+import {Spin} from 'antd'
+import './style.css'
+
 const Posts = ({setCurrentId,searchItem}) =>{
 
     const posts = useSelector((state) => state.posts);
+    // const posts = []
+    // console.log(posts);
+    // console.log(styles)
     return (
-        <div>
-        {posts?<div className="mt-11 mx-28 justify-center items-center">
+        <>
+        {posts?.length?<div className="mt-11 mx-28 justify-center items-center">
                     <Masonry columnsCount={3} gutter="20px" className="">
                         {!searchItem ? posts.map((post) =>
                             <Post post={post} key={post} setCurrentId={setCurrentId}/>) :
@@ -15,12 +21,11 @@ const Posts = ({setCurrentId,searchItem}) =>{
                                 <Post post={post} key={post} setCurrentId={setCurrentId}/>
                         )}
                     </Masonry>
-                </div>:<div>
-
-                    <h1>is loading</h1>
+                </div>:<div className="loading">
+                    <Spin tip="加载中" size="large"/>
                 </div>
         }
-        </div>
+        </>
     );
 
 }
